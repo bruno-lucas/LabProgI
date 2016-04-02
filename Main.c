9af279a971a35ee58b399191d56b1d027b3bc5dd
bruno-lucas <bruno.lucas@usp.br>
2016-04-02 14:18:09 -0300

    some fixes

--- Main.c
+++ Main.c
@@ -1,41 +1,45 @@
 #include<stdio.h>
 #include<stdlib.h>
+/*
 #include"cinematica.h"
 #include"resultante.h"
-
-struct Projectile {
+*/
+typedef struct{
     int posx;
     int posy;
     int velx;
     int vely;
-    float mass;
-};
+    double mass;
+}Projectile;
 
-struct Object {
+typedef struct {
    int life;
    char * name;
-   float radius;
+   double radius;
    int posx;
    int posy;
    int velx;
    int vely;
-   float mass;
+   double mass;
    int projectiles;
-};
+}Object;
 
+/*
 void update(){
 
 
 }
+*/
 
 int main(){
 
-Object *planeta;
-Object *nave1;
-Object *nave2;
-float time;
-float duration;
+Object * planeta;
+Object * nave1;
+Object * nave2;
+double time;
+double duration;
 Projectile *projectile;
+int projeteis;
 int i;
 
 /*Lê o arquivo e inicializa o programa*/
@@ -49,30 +53,30 @@ int i;
 
     /* Recebe a primeira linha */
     fscanf(arquivo, "%f %f %f",
-           &planeta.radius,
-           &planeta.mass,
+           &planeta->radius,
+           &planeta->mass,
            &time
            );
 
 
     /*Recebe a nave1*/
     fscanf(arquivo, "%s %f %d %d %d %d",
-           &nave1.name,
-           &nave1.mass,
-           &nave1.posx,
-           &nave1.posy,
-           &nave1.velx,
-           &nave1.vely
+           &nave1->name,
+           &nave1->mass,
+           &nave1->posx,
+           &nave1->posy,
+           &nave1->velx,
+           &nave1->vely
            );
 
    /*Recebe a nave2*/
     fscanf(arquivo, "%s %f %d %d %d %d",
-           &nave2.name,
-           &nave2.mass,
-           &nave2.posx,
-           &nave2.posy,
-           &nave2.velx,
-           &nave2.vely
+           &nave2->name,
+           &nave2->mass,
+           &nave2->posx,
+           &nave2->posy,
+           &nave2->velx,
+           &nave2->vely
            );
     /*Recebe a linha 3*/
     fscanf(arquivo, "%d %f",
@@ -86,17 +90,23 @@ int i;
 
     for(i = 0; i < projeteis; i++){
         fscanf("%f %f %f %f %f",
-               &projectile[i].mass,
-               &projectile[i].posx,
-               &projectile[i].posy,
-               &projectile[i].velx,
-               &projectile[i].vely
+               &projectile[i]->mass,
+               &projectile[i]->posx,
+               &projectile[i]->posy,
+               &projectile[i]->velx,
+               &projectile[i]->vely
                );
     }
 
 
 
     fclose(arquivo);
+
+    for(i = 0; i<projeteis; i++)
+        printf("%f %f %f %f %f \n", &projectile[i]->mass, &projectile[i]->posx, &projectile[i]->posy, &projectile[i]->velx, &projectile[i]->vely);
+    printf("%f \n", &nave1->posy);
+    printf("%f \n", &planeta->mass);
+    printf("%f \n", &nave2->velx);
 
 free(projectile);
 return 0;
