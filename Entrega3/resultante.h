2e6ad111b0abaf5ea143d02b6e4eec972819b998
dede999 <andreluizsa00@gmail.com>
2016-06-02 19:29:43 -0300

    Update resultante.h

--- Entrega3/resultante.h
+++ Entrega3/resultante.h
@@ -18,14 +18,15 @@
 
 #include <math.h>
 #define G 0.00000000667
+#define A 10.00
 
 /* recebe a posição e massa de um corpo1, posição e massa de outro corpo2 e calcula a força gravitacional no corpo 1*/
 /* (apenas intensidade) */
 double gravit(double x1, double y1, double m1, double x2, double y2, double m2);
 
 /* decompõe a força nos eixos x e y, respectivamente */
-double resx(double F, double x1, double y1, double x2, double y2, double acel);
+double resx(double F, double x1, double y1, double x2, double y2, int acel);
 
-double resy(double F, double x1, double y1, double x2, double y2, double acel);
+double resy(double F, double x1, double y1, double x2, double y2, int acel);
 
 #endif
