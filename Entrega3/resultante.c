9ef682175c3abf4dcb5d92d96570c06579450436
dede999 <andreluizsa00@gmail.com>
2016-06-02 18:51:33 -0300

    Create resultante.c

---
+++ Entrega3/resultante.c
@@ -0,0 +1,48 @@
+
+/*
+|---------------------------|
+|Laboratório de Programação |
+|---------------------------|
+ */
+
+
+/*
+|----------------------------------------------|
+|Bruno Guilherme Ricci Lucas      nºUSP 4460596
+|André Luiz Abdalla Silveira 	  nºUSP 8030353
+|Matheus Takeshi Yabiku		  nºUSP 7629949
+|----------------------------------------------|
+ */
+
+#include "Resultante.h"
+
+double gravit(double x1, double y1, double m1, double x2, double y2, double m2) {
+    double d;
+
+    d = pow((x2 - x1), 2) + pow((y2 - y1), 2);
+    return G*m1*m2/d;
+
+}
+
+/* decompõe a força nos eixos x e y, respectivamente */
+double resx(double F, double x1, double y1, double x2, double y2, double acel){ // veio do comando de (des)acelerar
+    double d;
+    double cos;
+
+    d = pow((x2 - x1), 2) + pow((y2 - y1), 2);
+    d = sqrt(d);
+    cos = (x2 - x1)/d;
+
+    return  cos * (F + acel);
+}
+
+double resy(double F, double x1, double y1, double x2, double y2, double acel){
+    double d;
+    double sen;
+
+    d = pow((x2 - x1), 2) + pow((y2 - y1), 2);
+    d = sqrt(d);
+    sen = (y2 - y1)/d;
+
+    return sen * (F + acel);
+}
